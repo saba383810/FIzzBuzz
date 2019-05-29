452bffc369b03f4cfde8912e7aedb0e2859ec3e4
saba383810 <saba383810@gmail.com>
2019-05-29 13:27:39 +0900

    programming of C lang 02

---
+++ FizzBuzz.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (){
+
+	int in_num,cnt;
+
+	printf("1~100までの値を入力してください >>");
+	scanf("%d",&in_num);
+	if(1 <= in_num && in_num <=100){
+		
+		for(cnt = 1;cnt <= in_num;cnt++){
+	
+			if(cnt % 3 == 0 && cnt % 5 == 0){
+					
+				printf("FizzBuzz!\n");
+
+			}else if (cnt % 3 == 0){
+		
+				printf("Fizz!\n");
+		
+			}else if (cnt % 5 == 0){
+		
+				printf("Buzz!\n");
+		
+			}else{
+		
+				printf("%d\n",cnt);
+		
+			}
+		}
+	}
+}
